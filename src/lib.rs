fdf520b0e4e2dcf82ae898e241459b4721bc32aa
Jacob Lifshay <programmerjake@gmail.com>
2019-11-03 22:30:10 -0800

    finish deleting old test cases

--- src/lib.rs
+++ src/lib.rs
@@ -29,13 +29,6 @@ use std::ops::ShlAssign;
 use std::ops::Shr;
 use std::ops::ShrAssign;
 
-// FIXME: re-add test cases
-// #[cfg(test)]
-// mod binary_op_test_cases;
-// #[cfg(test)]
-// mod from_real_algebraic_number_test_cases;
-// #[cfg(test)]
-// mod mul_add_test_cases;
 #[cfg(test)]
 mod test_cases;
 
